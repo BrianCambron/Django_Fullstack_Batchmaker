f7c0262bbdc94682479ba021b4d560005ef2407d
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-30 11:12:51 -0400

    added some styling to full recipe

--- frontend/static/src/components/FullRecipe.js
+++ frontend/static/src/components/FullRecipe.js
@@ -15,6 +15,11 @@ class FullRecipe extends Component {
       <h5 style={{textAlign: 'center'}}>{this.props.pickedRecipe.name}</h5>
       <img src={this.props.pickedRecipe.image} alt=""/>
       <div className='p-4'>
+        <div style={{display:'flex', justifyContent: 'space-around'}}>
+          <p>Prep time: {this.props.pickedRecipe.prep_time} Minutes</p>
+          <p>Cook Time: {this.props.pickedRecipe.cook_time} minutes</p>
+          <p>Amount: {this.props.pickedRecipe.amount}</p>
+        </div>
         <p>Ingredients you will need:</p>
         <p>{this.props.pickedRecipe.ingredients}</p>
         <p>Instructions:</p>
